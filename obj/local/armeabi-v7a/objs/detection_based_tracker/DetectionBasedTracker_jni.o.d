775018fbbaadd5c17d3a611c80017794a7ffdfe7
ALEX YI <alexyi1234@gmail.com>
2015-10-04 10:27:42 -0400

    Initial commit

---
+++ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,166 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/cvdef.h \
+ ../../sdk/native/jni/include/opencv2/hal/defs.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ ../../sdk/native/jni/include/opencv2/core/base.hpp \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/climits \
+ ../../sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ ../../sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ ../../sdk/native/jni/include/opencv2/hal.hpp \
+ ../../sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ ../../sdk/native/jni/include/opencv2/core/traits.hpp \
+ ../../sdk/native/jni/include/opencv2/core/matx.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types.hpp \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ ../../sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ ../../sdk/native/jni/include/opencv2/core/persistence.hpp \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
+ ../../sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ ../../sdk/native/jni/include/opencv2/core/utility.hpp \
+ ../../sdk/native/jni/include/opencv2/core/core_c.h \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h \
+ ../../sdk/native/jni/include/opencv2/core/optim.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/cvdef.h:
+
+../../sdk/native/jni/include/opencv2/hal/defs.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+../../sdk/native/jni/include/opencv2/core/base.hpp:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/climits:
+
+../../sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+../../sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+../../sdk/native/jni/include/opencv2/hal.hpp:
+
+../../sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+
+../../sdk/native/jni/include/opencv2/core/traits.hpp:
+
+../../sdk/native/jni/include/opencv2/core/matx.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types.hpp:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+../../sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+../../sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
+
+../../sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+../../sdk/native/jni/include/opencv2/core/utility.hpp:
+
+../../sdk/native/jni/include/opencv2/core/core_c.h:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h:
+
+../../sdk/native/jni/include/opencv2/core/optim.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+C:/Dev/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+C:/Dev/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h:
